81e9937c155a36395a6279d4f8ea83a0bf55b2de
MoneyBags-XIV <70113581+MoneyBags-XIV@users.noreply.github.com>
2024-12-25 21:40:37 +0000

    adds logic

--- puzzle.py
+++ puzzle.py
@@ -1,19 +1,36 @@
 class Puzzle:
     def __init__(self, grid):
         self.grid = grid
+        self.candidates = [[[x+1 for x in range(9) if self.could_be(j, k, x+1)] for k in range(9)] for j in range(9)]
+
     
     def solve_puzzle(self):
         while True:
             ans = self.grid
+            adsf = self.candidates
+
             self.solve_by_elimination()
             self.solve_by_box()
             self.solve_by_row()
             self.solve_by_column()
+            self.update_candidates()
 
-            if self.grid == ans:
+            print("\n")
+
+            if self.grid == ans and self.candidates == adsf:
                 return
     
+    def update_candidates(self):
+        for i, row in enumerate(self.grid):
+            for j, num in enumerate(row):
+                for k in self.candidates[i][j]:
+                    if not self.could_be(i, j, k):
+                        self.candidates[i][j].remove(k)
+
+    
     def solve_by_box(self):
+        iterations = 0
+        nums_found = 0
         while True:
             ans = self.grid
 
@@ -33,18 +50,53 @@ class Puzzle:
                             
                     if len(possible_indexes) == 1:
                         ans[possible_indexes[0][0]][possible_indexes[0][1]] = num + 1
+                        nums_found += 1
+                    
+                    elif len(possible_indexes) > 1:
+                        asdf = [x[0] for x in possible_indexes]
+                        jkl = [x[1] for x in possible_indexes]
+
+                        if len(set(asdf)) <= 1:
+                            for i in range(9):
+                                if i in jkl:
+                                    continue
+                                try:
+                                    self.candidates[asdf[0]][i].remove(num+1)
+                                except:
+                                    pass
+                        
+                        elif len(set(jkl)) <= 1:
+                            for i in range(9):
+                                if i in asdf:
+                                    continue
+                                try:
+                                    self.candidates[i][jkl[0]].remove(num+1)
+                                except:
+                                    pass
+
+            
+            iterations += 1
             
             if ans == self.grid:
+                print("Iterating by box found " + str(nums_found) + " numbers over " + str(iterations) + " iterations.")
+
                 return
             
             self.grid = ans
 
-    def solve_by_row(self):
+    def solve_by_row(self, column=False):
+        iterations = 0
+        nums_found = 0
         while True:
             ans = self.grid
 
+            # print(self.grid)
+
             for i, row in enumerate(self.grid):
+                # print(row)
                 for k in range(9):
+                    # print(str(k))
+                    # print(row)
                     if k+1 in row:
                         continue
                     possible_indexes = []
@@ -54,18 +106,47 @@ class Puzzle:
                     
                     if len(possible_indexes) == 1:
                         ans[i][possible_indexes[0]] = k+1
+                        nums_found += 1
+
+                    elif len(possible_indexes) > 1:
+                        adsf = [get_box(i, x) for x in possible_indexes]
+                        possible_indexes = [[i, x] for x in possible_indexes]
+
+                        if len(set(adsf)) <= 1:
+                            box_num = adsf[0]
+
+                            for i in range(3):
+                                for j in range(3):
+                                    rowjkl = 3*(box_num//3) + i
+                                    col = 3*(box_num%3) + j
+                                    if [rowjkl, col] in possible_indexes:
+                                        continue
+                                    try:
+                                        self.candidates[rowjkl, col].remove(k+1)
+                                    except:
+                                        pass
+
+            
+            iterations += 1
                 
             if ans == self.grid:
+                if column:
+                    print("Iterating by column found " + str(nums_found) + " numbers over " + str(iterations) + " iterations.")
+                else:
+                    print("Iterating by row found " + str(nums_found) + " numbers over " + str(iterations) + " iterations.")
+
                 return
             
             self.grid = ans
     
     def solve_by_column(self):
         self.flip()
-        self.solve_by_row()
+        self.solve_by_row(column=True)
         self.flip()
 
     def solve_by_elimination(self):
+        iterations = 0
+        nums_found = 0
         while True:
             ans = self.grid
 
@@ -78,8 +159,12 @@ class Puzzle:
                     
                     if len(could_be) == 1:
                         ans[i][j] = could_be[0]
+                        nums_found += 1
+            
+            iterations += 1
 
             if ans == self.grid:
+                print("Iterating by elimination found " + str(nums_found) + " numbers over " + str(iterations) + " iterations.")
                 return
             
             self.grid = ans
@@ -91,7 +176,16 @@ class Puzzle:
                 ans[j].append(num)
         self.grid = ans
 
+        ans = [[] for x in range(9)]
+        for i, row in enumerate(self.candidates):
+            for j, num in enumerate(row):
+                ans[j].append(num)
+        self.candidates = ans
+
     def could_be(self, row_number, column_number, number):
+        print(row_number)
+        print(column_number)
+        print(number)
         if self.number_in_column(number, column_number):
             return
         
@@ -104,6 +198,12 @@ class Puzzle:
         
         if self.grid[row_number][column_number] != 0:
             return
+        
+        try:
+            if not number in self.candidates[row_number][column_number]:
+                return
+        except:
+            pass
         
         return True
 
@@ -125,12 +225,15 @@ class Puzzle:
 
         for i, row in enumerate(self.grid):
             for num in row:
-                ans += "| " + str(num) + " "
+                if num != 0:
+                    ans += "| " + str(num) + " "
+                else:
+                    ans += "|   "
             ans += "|\n"
             if not (i+1)%3:
                 ans += ("+ - - - - - " * 3) + "+\n"
             else:
-                ans += ("- " * 18) + "\n"
+                ans += ("- " * 19) + "\n"
         
         return ans
 
