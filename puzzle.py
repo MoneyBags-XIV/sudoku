19f2065d8f555e36dca735aeb65a36a729004865
MoneyBags-XIV <70113581+MoneyBags-XIV@users.noreply.github.com>
2024-12-26 18:18:51 +0000

    Finished! Can do evil puzzles

--- puzzle.py
+++ puzzle.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Puzzle:
     def __init__(self, grid):
         self.grid = grid
@@ -32,12 +35,13 @@ class Puzzle:
         iterations = 0
         nums_found = 0
         while True:
-            ans = self.grid
+            ans = copy.deepcopy(self.grid)
 
             for box_num in range(9):
+                number_can_coords = []
                 for num in range(9):
-                    if self.number_in_box(num+1, box_num):
-                        continue
+                    # if self.number_in_box(num+1, box_num):
+                    #     continue
 
                     possible_indexes = []
 
@@ -47,6 +51,8 @@ class Puzzle:
                             col = 3*(box_num%3) + j
                             if self.could_be(row, col, num+1):
                                 possible_indexes.append([row, col])
+
+                    number_can_coords.append(tuple(possible_indexes))
                             
                     if len(possible_indexes) == 1:
                         ans[possible_indexes[0][0]][possible_indexes[0][1]] = num + 1
@@ -73,7 +79,24 @@ class Puzzle:
                                     self.candidates[i][jkl[0]].remove(num+1)
                                 except:
                                     pass
-
+                
+                for i, possible_coord in enumerate(number_can_coords):
+                    qwer = [possible_coord]
+                    uiop = [i+1]
+                    for j in range(9):
+                        if j == i:
+                            continue
+                        if tuple(number_can_coords[j]) == tuple(possible_coord):
+                            qwer.append(number_can_coords[j])
+                            uiop.append(j+1)
+                    if len(qwer) == len(possible_coord):
+                        for coord in possible_coord:
+                            for x in range(1, 10):
+                                if not x in uiop:
+                                    try:
+                                        self.candidates[coord[0]][coord[1]].remove(x)
+                                    except:
+                                        pass
             
             iterations += 1
             
@@ -88,21 +111,19 @@ class Puzzle:
         iterations = 0
         nums_found = 0
         while True:
-            ans = self.grid
-
-            # print(self.grid)
+            ans = copy.deepcopy(self.grid)
 
             for i, row in enumerate(self.grid):
-                # print(row)
+                number_can_coords = []
                 for k in range(9):
-                    # print(str(k))
-                    # print(row)
-                    if k+1 in row:
-                        continue
                     possible_indexes = []
                     for j, num in enumerate(row):
                         if self.could_be(i, j, k+1):
                             possible_indexes.append(j)
+                    
+                    ghghgh = [[i, x] for x in possible_indexes]
+                    
+                    number_can_coords.append(tuple(ghghgh))
                     
                     if len(possible_indexes) == 1:
                         ans[i][possible_indexes[0]] = k+1
@@ -125,6 +146,24 @@ class Puzzle:
                                         self.candidates[rowjkl, col].remove(k+1)
                                     except:
                                         pass
+            
+                for i, possible_coord in enumerate(number_can_coords):
+                    qwer = [possible_coord]
+                    uiop = [i+1]
+                    for j in range(9):
+                        if j == i:
+                            continue
+                        if tuple(number_can_coords[j]) == tuple(possible_coord):
+                            qwer.append(number_can_coords[j])
+                            uiop.append(j+1)
+                    if len(qwer) == len(possible_coord):
+                        for coord in possible_coord:
+                            for x in range(1, 10):
+                                if not x in uiop:
+                                    try:
+                                        self.candidates[coord[0]][coord[1]].remove(x)
+                                    except:
+                                            pass
 
             
             iterations += 1
@@ -148,7 +187,7 @@ class Puzzle:
         iterations = 0
         nums_found = 0
         while True:
-            ans = self.grid
+            ans = copy.deepcopy(self.grid)
 
             for i in range(len(self.grid)):
                 for j in range(len(self.grid[i])):
@@ -183,9 +222,6 @@ class Puzzle:
         self.candidates = ans
 
     def could_be(self, row_number, column_number, number):
-        print(row_number)
-        print(column_number)
-        print(number)
         if self.number_in_column(number, column_number):
             return
         
